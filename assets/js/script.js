54cc0c653c32ed1f502ee1e554f0aa195aa95a28
Gustavo Amaral <gustavo.ad.rego@unesp.br>
2021-01-14 22:57:19 -0300

    Setas não dão scroll! Fized

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,6 +96,13 @@ function atualizaTela() {
 setInterval(atualizaTela, 20);
 
 function leDoTeclado(evento) {
+  
+    window.addEventListener("keydown", function(e) {
+      // space and arrow keys
+      if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+          e.preventDefault();
+      }
+  }, false);
 
     if(evento.keyCode == cima) {
 
