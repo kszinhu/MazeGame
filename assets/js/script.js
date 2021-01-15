2db728820c42abd56a3f00e85e2b73758c2d5899
Matheus Ribeiro <matheus.ribeiro-silva@unesp.br>
2021-01-14 21:57:22 -0300

    animação base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,107 @@
-var body = document.querySelector(".objectbody");
-
-body.addEventListener("onclick", rotation());
-
-
-
 var tela = document.querySelector('canvas');
 var pincel = tela.getContext('2d');
-pincel.fillStyle = 'blue';
+pincel.fillStyle = 'lightgray';
 pincel.fillRect(0, 0, 600, 400);
-pincel.beginPath();
-pincel.fillRect(0,0,20,30);
-pincel.fill();
 
 var x = 20;
 var y = 20;
 
-var esquerda = 37;
-var cima = 38;
-var direita = 39;
-var baixo = 40;
+// códigos do teclado
+
+var esquerda = 37
+var cima = 38
+var direita = 39
+var baixo = 40
 var taxa = 10;
+
+function desenhaCirculo(x, y, raio) {
+
+  pincel.fillStyle = 'red';
+    pincel.beginPath();
+    pincel.arc(x+21, y+4, raio, 0, 2 * Math.PI);
+    pincel.fill();
+}
+
+
+function desenhaCirculo2(x, y, raio) {
+
+  pincel.fillStyle = 'grey';
+    pincel.beginPath();
+    pincel.arc(x+20, y, raio, 0, 2 * Math.PI);
+    pincel.fill();
+}
+
+function desenhaCirculo3(x, y, raio) {
+
+  pincel.fillStyle = 'grey';
+    pincel.beginPath();
+    pincel.arc(x+20, y+40, raio, 0, 2 * Math.PI);
+    pincel.fill();
+    
+}
+
+function desenhaRetangulo(x, y) {
+
+  pincel.fillStyle = 'lightgray';
+  pincel.beginPath();
+  pincel.fillRect(0, 0, 600, 400);
+ 
+}
 
 function limpaTela() {
 
-  pincel.clearRect(0, 0, 200, 100);
+    pincel.clearRect(0, 0, 600, 400);
 }
 
-function atualizaTela() {
-  limpaTela();
+function desenhaCirculo4(x, y, raio) {
+
+  pincel.fillStyle = 'black';
+    pincel.beginPath();
+    pincel.arc(x+7,y-8, 2, 0, 2 * Math.PI);
+    pincel.fill();
 }
+
+function desenhaCirculo5(x, y, raio) {
+
+  pincel.fillStyle = 'black';
+    pincel.beginPath();
+    pincel.arc(x+35, y-8, 2, 0, 2 * Math.PI);
+    pincel.fill();
+}
+
+
+function atualizaTela() {
+
+    limpaTela();
+    desenhaCirculo3(x, y, 30);
+    desenhaCirculo2(x, y, 30);
+    desenhaCirculo(x, y, 10);
+    desenhaCirculo4(x, y, 10);
+    desenhaCirculo5(x, y, 10);
+}
+
+
 
 setInterval(atualizaTela, 20);
 
 function leDoTeclado(evento) {
-  if (evento.keycode == cima) {
-    y = y - taxa;
-  }else if (evento.keyCode == baixo) {
-    y = y + taxa;
-  }else if (evento.keycode == esquerda) {
-    x = x - taxa;
-  } else if (evento.keycode == direita) {
-    x = x + taxa;
-  }
-}
-document.onkeydown = leDoTeclado; 
 
+    if(evento.keyCode == cima) {
+
+        y = y - taxa;
+
+    } else if (evento.keyCode == baixo) {
+
+        y = y + taxa;
+
+    } else if (evento.keyCode == esquerda) {
+
+        x = x - taxa;
+
+    } else if (evento.keyCode == direita) {
+
+        x = x + taxa;
+    }
+}
+
+document.onkeydown = leDoTeclado;
