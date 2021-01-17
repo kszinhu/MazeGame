44b550bede7e75c3fcba502b6dcdaf10b799f27e
Kszinhu <rodrigues.cassiano2007@gmail.com>
2021-01-17 16:32:07 -0300

    🆗 - Testing and "Initial Responsive"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var tela = document.querySelector('canvas');
-var pincel = tela.getContext('2d');
-pincel.fillStyle = 'lightgray';
-pincel.fillRect(0, 0,1175, 500);
+var tela = document.querySelector("canvas");
+var pincel = tela.getContext("2d");
+pincel.fillStyle = "lightgray";
+pincel.fillRect(0, 0, 1175, 500);
 
 var x = 25;
 var y = 25;
@@ -20,134 +20,113 @@ var taxa = 10;
 var Enter = 13;
 
 function desenhaCirculo(x, y, raio) {
-
-  pincel.fillStyle = 'red';
-    pincel.beginPath();
-    pincel.arc(x+21, y+4, raio, 0, 2 * Math.PI);
-    pincel.fill();
+  pincel.fillStyle = "red";
+  pincel.beginPath();
+  pincel.arc(x + 21, y + 4, raio, 0, 2 * Math.PI);
+  pincel.fill();
 }
 
-
 function desenhaCirculo2(x, y, raio) {
-
-  pincel.fillStyle = '#964b00';
-    pincel.beginPath();
-    pincel.arc(x+20, y, raio, 0, 2 * Math.PI);
-    pincel.fill();
+  pincel.fillStyle = "#964b00";
+  pincel.beginPath();
+  pincel.arc(x + 20, y, raio, 0, 2 * Math.PI);
+  pincel.fill();
 }
 
 function desenhaCirculo3(x, y, raio) {
-
-  pincel.fillStyle = '#964b00';
-    pincel.beginPath();
-    pincel.arc(x+20, y+40, raio, 0, 2 * Math.PI);
-    pincel.fill();
-    
+  pincel.fillStyle = "#964b00";
+  pincel.beginPath();
+  pincel.arc(x + 20, y + 40, raio, 0, 2 * Math.PI);
+  pincel.fill();
 }
 
 function desenhaRetangulo(x, y) {
-
-  pincel.fillStyle = 'black';
+  pincel.fillStyle = "black";
   pincel.beginPath();
-  pincel.fillRect( 0, 0, 1175, 500);
- 
+  pincel.fillRect(0, 0, 1175, 500);
 }
 
 function limpaTela() {
-
-    pincel.clearRect(0, 0, 1175, 500);
+  pincel.clearRect(0, 0, 1175, 500);
 }
 
 function desenhaCirculo4(x, y, raio) {
-
-  pincel.fillStyle = 'black';
-    pincel.beginPath();
-    pincel.arc(x+7,y-8, 2, 0, 2 * Math.PI);
-    pincel.fill();
+  pincel.fillStyle = "black";
+  pincel.beginPath();
+  pincel.arc(x + 7, y - 8, 2, 0, 2 * Math.PI);
+  pincel.fill();
 }
 
 function desenhaCirculo5(x, y, raio) {
-
-  pincel.fillStyle = 'black';
-    pincel.beginPath();
-    pincel.arc(x+35, y-8, 2, 0, 2 * Math.PI);
-    pincel.fill();
+  pincel.fillStyle = "black";
+  pincel.beginPath();
+  pincel.arc(x + 35, y - 8, 2, 0, 2 * Math.PI);
+  pincel.fill();
 }
 
-function desenhaTriangulo(x,y,z){
-
-pincel.beginPath();
-pincel.moveTo(x+50,y+25);
-pincel.lineTo(x+75,y+50);
-pincel.lineTo(x+75,y);
-pincel.fill();
+function desenhaTriangulo(x, y, z) {
+  pincel.beginPath();
+  pincel.moveTo(x + 50, y + 25);
+  pincel.lineTo(x + 75, y + 50);
+  pincel.lineTo(x + 75, y);
+  pincel.fill();
 }
-
-
 
 function atualizaTela() {
-
-    limpaTela();
-    desenhaCirculo3(x, y, 30);
-    desenhaCirculo2(x, y, 30);
-    desenhaCirculo(x, y, 10);
-    desenhaCirculo4(x, y, 10);
-    desenhaCirculo5(x, y, 10);
-    desenhaTriangulo(x,y,z);
+  limpaTela();
+  desenhaCirculo3(x, y, 30);
+  desenhaCirculo2(x, y, 30);
+  desenhaCirculo(x, y, 10);
+  desenhaCirculo4(x, y, 10);
+  desenhaCirculo5(x, y, 10);
+  desenhaTriangulo(x, y, z);
 }
 
 var big = false;
 
 setInterval(atualizaTela, 20);
 
-function leDoTeclado(evento){
-  
-  window.addEventListener("keydown", function(e){
+function leDoTeclado(evento) {
+  window.addEventListener("keydown", function (e) {
     // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
     }
-    window.onkeydown = function(e) {
-      if(e.keyCode == 13 && big == false) {
+    window.onkeydown = function (e) {
+      if (e.keyCode == 13 && big == false) {
         // console.log("BOM DIA!"); COLOCA O SCRIPT DE DIMINUIR O TATU AQUI!
         big = true;
-      }
-      else {
+      } else {
         // console.log("BOA NOITE!"); COLOCA O SCRIPT DE AUMENTAR O TATU AQUI!
-        big = false
+        big = false;
       }
-    }
+    };
   });
 
-  
+  //   window.onmousedown = function() {
 
-//   window.onmousedown = function() {
+  //     console.log("BOM DIA");
+  // }
 
-//     console.log("BOM DIA");
-// }
+  // // atribuindo diretamente a função anônima
+  //  window.onmouseup = function() {
 
-// // atribuindo diretamente a função anônima
-//  window.onmouseup = function() {
+  //     console.log("BOA NOITE");
+  // }
 
-//     console.log("BOA NOITE");
-// }
-
-    if(evento.keyCode == cima) {
-
-        y = y - taxa;
-
-    } else if (evento.keyCode == baixo) {
-
-        y = y + taxa;
-
-    } else if (evento.keyCode == esquerda) {
-
-        x = x - taxa;
-
-    } else if (evento.keyCode == direita) {
-
-        x = x + taxa;
-    }
+  if (evento.keyCode == cima) {
+    y = y - taxa;
+    PopUp(1);
+  } else if (evento.keyCode == baixo) {
+    y = y + taxa;
+    PopUp(1);
+  } else if (evento.keyCode == esquerda) {
+    x = x - taxa;
+    PopUp(1);
+  } else if (evento.keyCode == direita) {
+    x = x + taxa;
+    PopUp(1);
+  }
 }
 
 document.onkeydown = leDoTeclado;
